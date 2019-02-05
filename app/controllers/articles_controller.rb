39305404e56fe73742ab48a9b93119c4587371d7
Nadia89M <nadia.mohamed89@gmail.com>
2019-02-05 14:40:15 +0000

    handle exception for article not found

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,14 @@ class ArticlesController < ApplicationController
   def article_params
      params.require(:article).permit(:title, :body) 
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found 
+
+  protected
+
+  def resource_not_found
+    message = "The article you are looking for could not be found" 
+    flash[:alert] = message
+    redirect_to root_path
+  end
 end
